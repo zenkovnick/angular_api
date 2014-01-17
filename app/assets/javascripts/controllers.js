f9f32c3c34707b45505c5e58a0da61e6679091e5
Mykola Zenkov <mykola.zenkov@masterofcode.com>
2014-01-17 17:51:22 +0200

    Init commit

---
+++ app/assets/javascripts/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+//    $http.get('http://localhost:3000/list').success(function(data) {
+//        $scope.phones = data;
+//    });
+    $scope.phones = [
+        {"name": "Nexus S",
+            "snippet": "Fast just got faster with Nexus S."},
+        {"name": "Motorola XOOM™ with Wi-Fi",
+            "snippet": "The Next, Next Generation tablet."},
+        {"name": "MOTOROLA XOOM™",
+            "snippet": "The Next, Next Generation tablet."}
+    ];
+    $scope.orderProp = 'age';
+
+});
+//PhoneListCtrl.$inject = ['$scope', '$http'];
---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,14 @@
+class PhonesController < ApplicationController
+  #layout "application"
+
+  def index
+    render "index"
+  end
+  def list
+    render json: File.read("lib/phones/phones.json"), :callback => params[:callback]
+  end
+
+  def details
+    render json: File.read("lib/phones/#{params[:model]}.json"), :callback => params[:callback]
+  end
+end
